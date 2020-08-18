3e35b99efa8cb3a602824866d440ba1e578f0092
EJ Silva <silvajr.efren@g.batastate-u.edu.ph>
2020-08-19 00:56:01 +0800

    edit database

--- database-server/database.sql
+++ database-server/database.sql
@@ -1,218 +1,40 @@
--- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
---
--- Host: localhost    Database: server
--- ------------------------------------------------------
--- Server version	8.0.21
+CREATE TABLE `chat` (
+    `chatID` INT PRIMARY KEY AUTO_INCREMENT,
+    `userID` INT,
+    `chatroomID` INT,
+    `message` VARCHAR(100),
+    `chat_date` DATE
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE `chatroom` (
+    `chatroomID` INT PRIMARY KEY AUTO_INCREMENT,
+    `room_name` VARCHAR(15),
+    `date_created` DATE,
+    `chat_password` VARCHAR(10),
+    `userID` INT
+);
 
---
--- Table structure for table `branch`
---
+CREATE TABLE `user` (
+    `userID` INT AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(15) UNIQUE,
+    `password` VARCHAR(10),
+    `first_name` VARCHAR(20),
+    `last_name` VARCHAR(20),
+    `access` BOOLEAN
+);
 
-DROP TABLE IF EXISTS `branch`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `branch` (
-  `branch_id` int NOT NULL AUTO_INCREMENT,
-  `branch_name` varchar(20) DEFAULT NULL,
-  `mgr_id` int DEFAULT NULL,
-  PRIMARY KEY (`branch_id`),
-  KEY `mgr_id` (`mgr_id`),
-  CONSTRAINT `branch_ibfk_1` FOREIGN KEY (`mgr_id`) REFERENCES `employee` (`emp_id`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO `chatroom` (`chatroomID`, `room_name`, `date_created`, `chat_password`, `userID`) VALUES
+(1, 'My First Chat Room', '2017-09-11', 'suckdick', 2),
+(2, 'Free Entrance :)', '2017-09-11', '', 3),
+(3, 'Admin Chat Room', '2017-09-11', '', 1);
 
---
--- Dumping data for table `branch`
---
+INSERT INTO `user` (`userID`, `username`, `password`, `first_name`, `last_name`, `access`) VALUES
+(1, 'admin', 'admin123', 'John', 'Doe', true),
+(2, 'user01', 'password123', 'Albert', 'Einstein', false),
+(3, 'user02', 'password123', 'Isaac', 'Newton', false);
 
-LOCK TABLES `branch` WRITE;
-/*!40000 ALTER TABLE `branch` DISABLE KEYS */;
-/*!40000 ALTER TABLE `branch` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `employee`
---
-
-DROP TABLE IF EXISTS `employee`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `employee` (
-  `emp_id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(20) DEFAULT NULL,
-  `last_name` varchar(20) DEFAULT NULL,
-  `password` varchar(15) DEFAULT NULL,
-  `super_id` int DEFAULT NULL,
-  `branch_id` int DEFAULT NULL,
-  PRIMARY KEY (`emp_id`),
-  KEY `branch_id` (`branch_id`),
-  KEY `super_id` (`super_id`),
-  CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `branch` (`branch_id`) ON DELETE SET NULL,
-  CONSTRAINT `employee_ibfk_2` FOREIGN KEY (`super_id`) REFERENCES `employee` (`emp_id`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `employee`
---
-
-LOCK TABLES `employee` WRITE;
-/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `internet_speed`
---
-
-DROP TABLE IF EXISTS `internet_speed`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `internet_speed` (
-  `emp_id` int DEFAULT NULL,
-  `date` date DEFAULT NULL,
-  `speed` float DEFAULT NULL,
-  KEY `emp_id` (`emp_id`),
-  CONSTRAINT `internet_speed_ibfk_1` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `internet_speed`
---
-
-LOCK TABLES `internet_speed` WRITE;
-/*!40000 ALTER TABLE `internet_speed` DISABLE KEYS */;
-/*!40000 ALTER TABLE `internet_speed` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `mails`
---
-
-DROP TABLE IF EXISTS `mails`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `mails` (
-  `mail_id` int NOT NULL AUTO_INCREMENT,
-  `emp_id` int DEFAULT NULL,
-  `subject` varchar(20) DEFAULT NULL,
-  `content` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`mail_id`),
-  KEY `emp_id` (`emp_id`),
-  CONSTRAINT `mails_ibfk_1` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `mails`
---
-
-LOCK TABLES `mails` WRITE;
-/*!40000 ALTER TABLE `mails` DISABLE KEYS */;
-/*!40000 ALTER TABLE `mails` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `task`
---
-
-DROP TABLE IF EXISTS `task`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `task` (
-  `task_id` int NOT NULL AUTO_INCREMENT,
-  `task_name` varchar(20) DEFAULT NULL,
-  `emp_id` int DEFAULT NULL,
-  `type_id` char(1) DEFAULT NULL,
-  `time_to_finish` int DEFAULT NULL,
-  PRIMARY KEY (`task_id`),
-  KEY `emp_id` (`emp_id`),
-  KEY `type_id` (`type_id`),
-  CONSTRAINT `task_ibfk_1` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`) ON DELETE SET NULL,
-  CONSTRAINT `task_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `task_type` (`type_id`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `task`
---
-
-LOCK TABLES `task` WRITE;
-/*!40000 ALTER TABLE `task` DISABLE KEYS */;
-/*!40000 ALTER TABLE `task` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `task_type`
---
-
-DROP TABLE IF EXISTS `task_type`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `task_type` (
-  `type_id` char(1) NOT NULL,
-  `branch_id` int DEFAULT NULL,
-  `type_descr` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`type_id`),
-  KEY `branch_id` (`branch_id`),
-  CONSTRAINT `task_type_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `branch` (`branch_id`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `task_type`
---
-
-LOCK TABLES `task_type` WRITE;
-/*!40000 ALTER TABLE `task_type` DISABLE KEYS */;
-/*!40000 ALTER TABLE `task_type` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `time`
---
-
-DROP TABLE IF EXISTS `time`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `time` (
-  `emp_id` int DEFAULT NULL,
-  `date` date DEFAULT NULL,
-  `time_in` time DEFAULT NULL,
-  `time_out` time DEFAULT NULL,
-  KEY `emp_id` (`emp_id`),
-  CONSTRAINT `time_ibfk_1` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `time`
---
-
-LOCK TABLES `time` WRITE;
-/*!40000 ALTER TABLE `time` DISABLE KEYS */;
-/*!40000 ALTER TABLE `time` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-08-13 12:01:06
+INSERT INTO `chat` (`chatID`, `userID`, `chatroomID`, `message`, `chat_date`) VALUES
+(1, 2, 1, 'Hello World', '2020-06-12'),
+(2, 3, 1, 'Hello World2', '2020-06-12')
+(3, 1, 1, 'Hello World3', '2020-06-12')
+(4, 1, 1, 'Hello World4', '2020-06-12');
