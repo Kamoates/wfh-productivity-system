8db7f4488c7a35da92dd15b02026c37eedbb0398
EJ Silva <silvajr.efren@g.batastate-u.edu.ph>
2020-08-20 23:23:58 +0800

    change html file to php

---
+++ final-php-server/pages/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" type="text/css" href="../stylesheets/login.css" />
+  <title>Login</title>
+</head>
+
+<body>
+  <div class="contents">
+    <img src="../src/logo-walrustech-no-bg.png" class="center" />
+    <div id="warning"></div>
+    <form id="user-info">
+      <input type="text" name="username" id="username" placeholder="username" />
+      <br />
+      <input type="password" name="password" id="password" placeholder="password" />
+      <br />
+      <input id="signin" type="submit" value="Sign In" />
+    </form>
+  </div>
+  <div class="footer">
+    <p>&copy; 2020 Walrus</p>
+  </div>
+  <script src="../scripts/retrieve-login-info.js"></script>
+</body>
+
+</html>
